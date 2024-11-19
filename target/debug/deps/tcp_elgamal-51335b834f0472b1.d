874bffbaffc8aaf047c2cc2ff00ca2e552ffa58a
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2024-11-19 11:14:01 -0500

    remove target

--- target/debug/deps/tcp_elgamal-51335b834f0472b1.d
+++
@@ -1,5 +0,0 @@
-/home/inferno/elgmal_tcp/target/debug/deps/libtcp_elgamal-51335b834f0472b1.rmeta: src/main.rs
-
-/home/inferno/elgmal_tcp/target/debug/deps/tcp_elgamal-51335b834f0472b1.d: src/main.rs
-
-src/main.rs:
--- target/debug/tcp_elgamal.d
+++
@@ -1,1 +0,0 @@
-/home/inferno/elgmal_tcp/target/debug/tcp_elgamal: /home/inferno/elgmal_tcp/lib/lib.rs /home/inferno/elgmal_tcp/src/main.rs
